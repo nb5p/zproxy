15239df0a180b0d4073fea7900908d03b013fba7
nb5p <2098464+nb5p@users.noreply.github.com>
2019-10-17 17:17:09 +0800

    change to zsh-style

---
+++ zproxy.zsh
@@ -0,0 +1,60 @@
+#!/bin/zsh
+
+function listOpt() {
+
+}
+
+function infoOpt() {
+    echo "公网IP：" "$( curl -L "ip.cn" )"
+    echo "内网IP：" "$( ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' )"
+}
+
+function helpOpt() {
+    
+}
+
+function shellProxy() {
+
+}
+
+function sshProxy() {
+
+}
+
+function gitProxy() {
+
+}
+
+function goProxy(){
+
+}
+
+case $1 {
+    (list)
+    echo "@list"
+    ;;
+
+    (info)
+    infoOpt
+    ;;
+
+    (help)
+    echo "@help"
+    ;;
+
+    (shell)
+    echo "@shell"
+    ;;
+
+    (ssh)
+    echo "@ssh"
+    ;;
+
+    (git)
+    echo "@git"
+    ;;
+
+    (go)
+    echo "@go"
+    ;;
+}
