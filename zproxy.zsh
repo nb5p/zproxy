6c32a4fb84fdba38b3f879a0cea2b989ff6cc7e9
nb5p <2098464+nb5p@users.noreply.github.com>
2019-10-24 17:29:26 +0800

    new feature shell list

--- zproxy.zsh
+++ zproxy.zsh
@@ -4,11 +4,19 @@ function listOpt() {
 
 }
 
-function infoOpt() {
-    echo "公网IP："
+function outOpt() {
     curl -s ip.sb
-    echo "内网IP："
+}
+
+function inOpt() {
     ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
+}
+
+function ipOpt() {
+    echo "Public IP:"
+    outOpt
+    echo "Intranet IP:"
+    inOpt
 }
 
 function helpOpt() {
@@ -16,7 +24,14 @@ function helpOpt() {
 }
 
 function shellProxy() {
-
+    case $1 {
+        (list)
+        [[ ""$HTTP_PROXY"" == "" && "$HTTPS_PROXY" == "" && "$ALL_PROXY" == "" ]] && echo "UNSET"
+        [[ "$HTTP_PROXY" != "" ]] && echo "HTTP_PROXY="$HTTP_PROXY
+        [[ "$HTTPS_PROXY" != "" ]] && echo "HTTPS_PROXY="$HTTPS_PROXY
+        [[ "$ALL_PROXY" != "" ]] && echo "ALL_PROXY="$ALL_PROXY
+        ;;
+    }
 }
 
 function sshProxy() {
@@ -36,8 +51,16 @@ case $1 {
     echo "@list"
     ;;
 
-    (info)
-    infoOpt
+    (ipi)
+    inOpt
+    ;;
+
+    (ipe)
+    outOpt
+    ;;
+
+    (ip)
+    ipOpt
     ;;
 
     (help)
@@ -45,7 +68,7 @@ case $1 {
     ;;
 
     (shell)
-    echo "@shell"
+    shellProxy $2
     ;;
 
     (ssh)
