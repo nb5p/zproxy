403b8fd19f96fb4825f3aed7082c567c05ba56ef
nb5p <2098464+nb5p@users.noreply.github.com>
2019-10-29 14:18:37 +0800

    Format code

--- zproxy.zsh
+++ zproxy.zsh
@@ -1,32 +1,17 @@
 #!/bin/zsh
 
-function listOpt() {
-
-}
-
-function outOpt() {
-    curl -s ip.sb
-}
-
-function inOpt() {
-    ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
-}
-
-function ipOpt() {
-    echo "Public IP:"
-    outOpt
-    echo "Intranet IP:"
-    inOpt
-}
-
-function helpOpt() {
-    
-}
+# Get IP {{{
+function outOpt() { curl -s ip.sb }
+function inOpt() { ifconfig \
+    | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' }
+function ipOpt() { echo "Public IP:"; outOpt; echo "Intranet IP:"; inOpt }
+# }}}
 
 function shellProxy() {
     case $1 {
         (list)
-        [[ ""$HTTP_PROXY"" == "" && "$HTTPS_PROXY" == "" && "$ALL_PROXY" == "" ]] && echo "UNSET"
+        [[ ""$HTTP_PROXY"" == "" && "$HTTPS_PROXY" == "" && "$ALL_PROXY" == "" ]] \
+            && echo "UNSET PROXY"
         [[ "$HTTP_PROXY" != "" ]] && echo "HTTP_PROXY="$HTTP_PROXY
         [[ "$HTTPS_PROXY" != "" ]] && echo "HTTPS_PROXY="$HTTPS_PROXY
         [[ "$ALL_PROXY" != "" ]] && echo "ALL_PROXY="$ALL_PROXY
@@ -37,49 +22,19 @@ function shellProxy() {
 function sshProxy() {
 
 }
+# }}}
 
-function gitProxy() {
-
-}
-
-function goProxy(){
-
-}
+# Get config file location {{{
+if [[ ! -f "./.zproxyrc" ]] { (($+XDG_CONFIG_HOME)) \
+    && configFile="$XDG_CONFIG_HOME/zproxy/zproxyrc" \
+    || configFile="$HOME/.config/zproxy/zproxyrc"
+} else { configFile="./.zproxyrc" }
+# }}}
 
 case $1 {
-    (list)
-    echo "@list"
-    ;;
-
-    (ipi)
-    inOpt
-    ;;
-
-    (ipe)
-    outOpt
-    ;;
-
-    (ip)
-    ipOpt
-    ;;
-
-    (help)
-    echo "@help"
-    ;;
-
-    (shell)
-    shellProxy $2
-    ;;
-
-    (ssh)
-    echo "@ssh"
-    ;;
-
-    (git)
-    echo "@git"
-    ;;
-
-    (go)
-    echo "@go"
-    ;;
+    (ipi) inOpt ;;
+    (ipe) outOpt ;;
+    (ip) ipOpt ;;
+    (shell) shellProxy $2 ;;
+    (edit) editConfig ;;
 }
