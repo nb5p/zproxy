59f48e5cac7eb8c0502721b317cfdcda261cae09
nb5p <2098464+nb5p@users.noreply.github.com>
2019-12-05 16:22:32 +0800

    feat. add command test

--- zproxy.zsh
+++ zproxy.zsh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/env zsh
 # set -x
 
 # Get IP {{{
@@ -10,6 +10,8 @@ function ipOpt() { echo "Public IP:"; outOpt; echo "Intranet IP:"; inOpt }
 
 # Handle Shell {{{
 function shellProxy() {
+    (( ${+shell} )) || { echo "Config Error, with \e[31;1mshell\e[0m";
+        exit 11 };
     case $1 {
         (list)
             [[ "$HTTP_PROXY" == "" && "$HTTPS_PROXY" == "" && "$ALL_PROXY" == "" ]] \
@@ -20,16 +22,20 @@ function shellProxy() {
         ;;
         (off)
             echo "Please run:"
-            echo -n "\e[31;1m"
+            echo -n "\e[33;1m"
             echo -n "  unset HTTP_PROXY HTTPS_PROXY ALL_PROXY"
             echo "\e[0m"
         ;;
         (on) ;;
         (*)
+            (( $shell[(I)$1] )) || { echo "Config Error, with \e[31;1mshell > $1\e[0m";
+                exit 12 }
             hport=`getValue ${1} http`
             sport=`getValue ${1} socks`
+            [[ "$hport" == "" && "$sport" == "" ]] && {
+                echo "Config Error, with \e[31;1mshell > $1 > http|socks\e[0m"; exit 13 }
             echo "Please run:"
-            echo -n "\e[31;1m"
+            echo -n "\e[33;1m"
             echo -n "  export HTTP_PROXY=http://localhost:${hport}; "
             echo -n "export HTTPS_PROXY=http://localhost:${hport}; "
             echo -n "export ALL_PROXY=socks5://localhost:${sport}"
@@ -41,12 +47,20 @@ function shellProxy() {
 
 # Handle NPM {{{
 function npmMirrors() {
+    { command -v npm } || { echo "Command Error, with \e[31;1mnpm\e[0m"; exit 31 }
+    (( ${+npm} )) || { echo "Config Error, with \e[31;1mnpm\e[0m"; exit 11 };
     case $1 {
         (list) npm config list ;;
         (off) npm config set registry https://registry.npmjs.org/ ;;
         (on) ;;
         (*)
+            (( ${+1} )) || { echo "Parameter Error, with \e[31;1mnpm\e[0m";
+                exit 21 }
+            (( $npm[(I)$1] )) || { echo "Config Error, with \e[31;1mnpm > $1\e[0m";
+                exit 12 }
             mirror=`getValue ${1} npm`
+            [[ "$mirror" == "" ]] && {
+                echo "Config Error, with \e[31;1mnpm > $1 > mirrors\e[0m"; exit 13 }
             npm config set registry $mirror
         ;;
     }
@@ -55,12 +69,20 @@ function npmMirrors() {
 
 # Handle PIP {{{
 function pipMirrors() {
+    { command -v pip } || { echo "Command Error, with \e[31;1mpip\e[0m"; exit 31 }
+    (( ${+pip} )) || { echo "Config Error, with \e[31;1mpip\e[0m"; exit 11 };
     case $1 {
         (list) pip3 config get global.index-url ;;
         (off) pip3 config unget global.index-url ;;
         (on) ;;
         (*)
+            (( ${+1} )) || { echo "Parameter Error, with \e[31;1mpip\e[0m";
+                exit 21 }
+            (( $pip[(I)$1] )) || { echo "Config Error, with \e[31;1mpip > $1\e[0m";
+                exit 12 }
             mirror=`getValue ${1} pip`
+            [[ "$mirror" == "" ]] && {
+                echo "Config Error, with \e[31;1mnpm > $1 > mirrors\e[0m"; exit 13 }
             pip3 config set global.index-url $mirror
         ;;
     }
@@ -71,7 +93,7 @@ function pipMirrors() {
 function handleConfig() {
     case $1 {
         (edit)
-            [[ "$+EDITOR" ]] && $EDITOR $configFile || vi $configFile
+            (( ${+EDITOR} )) && $EDITOR $configFile || vi $configFile
         ;;
         (list)
             cat $configFile
@@ -98,13 +120,9 @@ case $1 {
     (ip) ipOpt ;;
 
     (shell) shellProxy $2 ;;
-    (ssh) sshProxy $2 ;;
-    (git) gitProxy $2 ;;
 
     (npm) npmMirrors $2 ;;
     (pip) pipMirrors $2 ;;
-    (brew) brewMirrors $2 ;;
-    (conda) condaMirrors $2 ;;
 
     (config) handleConfig $2 ;;
 }
